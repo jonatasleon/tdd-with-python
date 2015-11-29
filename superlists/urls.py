cf41a22c4a525c84d2eee67a82cbe660184bbaf7
Jonatas Leon <jonatas.leon@gmail.com>
2015-11-29 01:57:14 -0200

    For cover PEP8

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^admin/', include(admin.site.urls)),
